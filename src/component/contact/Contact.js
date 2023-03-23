0dffd06f5b2d8d9e526e4d2ad566adad39dfefe5
ulanbekov1 <musabaibolot@gmail.com>
2023-03-23 15:57:15 +0300

    improvements

--- src/component/contact/Contact.js
+++ src/component/contact/Contact.js
@@ -2,7 +2,10 @@ import React from 'react';
 import "./Contact.scss"
 import "./Media.scss"
 import ContactIcon from "./img/Frame 10.svg"
-import {BsArrowRightShort, BsInstagram, BsTelegram} from "react-icons/bs"
+import {BsArrowRightShort, BsFillTelephoneOutboundFill, BsInstagram, BsTelegram} from "react-icons/bs"
+import {HiOutlineMailOpen} from "react-icons/hi"
+import {FaTelegramPlane} from "react-icons/fa"
+import {GrInstagram} from "react-icons/gr"
 
 
 const Contact = () => {
@@ -25,26 +28,37 @@ const Contact = () => {
                         <p>Opening hours:</p>
                         <h3>Mon-Thu: 10:00 am - 01:00 <br/> am</h3>
                         <h3>Fri-Sun: 10:00 am - 02:00 am</h3>
-
-                        <button className="contact--text__btn">Purchase gift card <BsArrowRightShort/></button>
+<div style={{
+    paddingTop: '30px'
+}}>
+    <hr style={{
+        width:  "130px",
+        border: "1px solid red",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "10px 0 5px 45px",
+    }}/>
+    <button className="contact--text__btn">Purchase gift card <BsArrowRightShort/></button>
+    <hr style={{
+        width:  "130px",
+        height: "1px",
+        margin: "5px 0",
+        border: "1px solid red",
+    }}/>
+</div>
                     </div>
 
                     <div className="contact__maps">
-                        <a className="dg-widget-link"
-                           href="http://2gis.kg/bishkek/firm/70000001057860881/center/74.5860829352023,42.880134176382036/zoom/17?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=bigMap">Посмотреть
-                            на карте Бишкека</a>
-                        <div className="dg-widget-link"><a
-                            href="http://2gis.kg/bishkek/firm/70000001057860881/photos/70000001057860881/center/74.5860829352023,42.880134176382036/zoom/17?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=photos">Фотографии
-                            компании</a></div>
-                        <div className="dg-widget-link"><a
-                            href="http://2gis.kg/bishkek/center/74.586003,42.87992/zoom/17/routeTab/rsType/bus/to/74.586003,42.87992╎Motion Web, IT-компания?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=route">Найти
-                            проезд до Motion Web, IT-компания</a></div>
-                        <div className="contact__maps--icons">
-                            <a href="https://t.me/Motion web"><BsTelegram/></a>
-                            <a href="https://instagram.com/motion_web?igshid=YmMyMTA2M2Y="><BsInstagram/></a>
+                        <div className="contact__maps--group">
+                            <h3>Contact Info</h3>
+                            <a href=""><b><BsFillTelephoneOutboundFill/></b>+771219900</a> <br/>
+                            <a href=""><b><HiOutlineMailOpen/></b>motionweb312@gmail.com</a> <br/>
+                            <a className="contact__maps--a" href="https://t.me/Motion web"><BsTelegram/></a>
+                            <a className="contact__maps--a" href="https://instagram.com/motion_web?igshid=YmMyMTA2M2Y="><BsInstagram/></a>
                         </div>
                         <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d12839.157435911387!2d74.57739742707425!3d42.88193397712214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2skg!4v1679482261675!5m2!1sru!2skg"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2196.8549415310567!2d74.58520831303298!3d42.880102918608614!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389ec81915eaf75d%3A0x9bfedde3851dcd21!2sMaximum!5e0!3m2!1sru!2skg!4v1679570014068!5m2!1sru!2skg"
                             allowFullScreen="" loading="lazy"
                             referrerPolicy="no-referrer-when-downgrade"></iframe>
                     </div>
