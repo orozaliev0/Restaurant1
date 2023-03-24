929d48e29fe41e9c0087f95fd81de094183ad3ae
Abdubakirovdev <rahimovich77kg>
2023-03-24 17:11:08 +0300

    oiouyghsgsefefzfv

--- src/component/hero/Hero.js
+++ src/component/hero/Hero.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Hero.scss"
 import vectorLeft from "./img/Vector left.png"
 import vectorRight from "./img/Vector right.png"
@@ -6,8 +6,15 @@ import vector from "./img/vector.png"
 import {IoMdArrowRoundForward} from "react-icons/io"
 import {GoLocation} from "react-icons/go"
 import {BsFillTelephoneFill} from "react-icons/bs"
+import {AiOutlineMenu} from "react-icons/ai";
+import {IoMdContact} from "react-icons/io"
+import {FaPhoneSquareAlt} from "react-icons/fa"
 
 const Hero = () => {
+
+    const [modal,setModal] = useState(false)
+
+
     return (
         <div>
             <section id="hero">
@@ -18,13 +25,21 @@ const Hero = () => {
                     <div className="heroo">
                         <div className="heroo-title">
                             <div className={"vector-left"}>
-                                <img src={vectorLeft} alt="img"/>
-                                <img src={vector} alt="img"/>
+                                <img style={
+                                    {width: "50px"}
+                                } src={vectorLeft} alt="img"/>
+                                <img style={
+                                    {width: "70px"}
+                                } src={vector} alt="img" className="vector-left-bottom"/>
                             </div>
                             <h1>Delicious</h1>
                             <div className={"vector-right"}>
-                                <img src={vectorRight} alt="img"/>
-                                <img src={vector} alt="img"/>
+                                <img style={
+                                              {width: "50px"}
+                                          }  src={vectorRight} alt="img"/>
+                                <img style={
+                                    {width: "70px"}
+                                } src={vector} alt="img" className="vector-left-bottom"/>
                             </div>
                         </div>
                         <div className="heroo-nav">
@@ -33,7 +48,30 @@ const Hero = () => {
                                 The Restaurant`s sunny decor was inspired by the diners</p>
                             <div className="hero-button">
                                 <div className="btn-top"></div>
-                                <button>Reserve Your  <IoMdArrowRoundForward  className="hero-right"/></button>
+                                <button onClick={() => setModal(!modal)}>Reserve Your  <IoMdArrowRoundForward  className="hero-right"/></button>
+                                <div className="modal" style={{display: modal? "block" : "none" }}>
+                                    <div className="modal-top">
+                                        <div className="modal-top_left">
+                                            <IoMdContact className="modal-icon"/>
+                                        </div>
+                                        <div className="modal-top_right">
+                                            <h6>Name</h6>
+                                            <h4>Enter your name</h4>
+                                        </div>
+                                    </div>
+                                    <div className="modal-bottom">
+                                        <div className="modal-top_left">
+                                            <FaPhoneSquareAlt className="modal-icon"/>
+                                        </div>
+                                        <div className="modal-top_right">
+                                            <h6>Phone</h6>
+                                            <h4>Enter your phone</h4>
+                                        </div>
+                                    </div>
+                                    <div className="modal-btn">
+                                        <button>Сontact</button>
+                                    </div>
+                                </div>
                                 <div className="btn-bottom"></div>
                             </div>
 
